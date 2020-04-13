e75620bc2eb31d86349584fe99c9e07323ecd211
Odinaka <agubataodinaka@gmail.com>
2020-04-13 03:50:46 +0100

    challenges 1-3

--- src/estimator.php
+++ src/estimator.php
@@ -1,47 +1,32 @@
 <?php
-function doCompute($input_array,$data_array,$input_data)
+/*$input = trim(file_get_contents("PHP://input"));
+$your_var = json_decode($input)*/
+function trunc($g)
 {
-    $input_array['data']=$data_array;
-    foreach($input_data as $x) {
-        if($x[2]==1)
-        {
-            $input_array[$x[0]][$x[1]]=intval(floor($data_array[$x[3][0]] * $x[4]));
-        }
-        elseif ($x[2]==2)
-        {
-            $input_array[$x[0]][$x[1]]=intval(floor($input_array[$x[3][0]][$x[3][1]]* $x[4]));
-        }
-        elseif ($x[2]==3)
-        {
-            $input_array[$x[0]][$x[1]]=intval(floor($data_array[$x[5]]-$input_array[$x[3][0]][$x[3][1]]));
-        }else {
-            $input_array[$x[0]][$x[1]]=intval(floor(($input_array[$x[3][0]][$x[3][1]]*$data_array[$x[3][2]][$x[3][3]]*$data_array[$x[3][2]][$x[3][4]])/$x[4]));
-        }
-    }
-    return $input_array;
+    if($g<0)$g=intval(ceil($g));
+    else $g=(int)floor($g);
+    return $g;
 }
 
 function covid19ImpactEstimator($data)
 {
-    $data1=array();
-    //$data=json_decode($data,true);
-    $t=array('days'=>1,'weeks'=>7,'month'=>30);
+    $data1=array('data'=>$data);
+    $t=array('days'=>1,'weeks'=>7,'months'=>30);
     $days=$data['timeToElapse']*$t[$data['periodType']];
-    $inp1=array(['impact','currentlyInfected',1,['reportedCases'],10],['severeImpact','currentlyInfected',1,['reportedCases'],50],
-        ['impact','infectionsByRequestedTime',2,['impact','currentlyInfected'],pow(2,$days/3)],
-        ['severeImpact','infectionsByRequestedTime',2,['severeImpact','currentlyInfected'],pow(2,$days/3)],
-        ['impact','severeCasesByRequestedTime',2,['impact','infectionsByRequestedTime'],0.15],
-        ['severeImpact','severeCasesByRequestedTime',2,['severeImpact','infectionsByRequestedTime'],0.15],
-        ['impact','hospitalBedsByRequestedTime',3,['impact','severeCasesByRequestedTime'],0.15,'totalHospitalBeds'],
-        ['severeImpact','hospitalBedsByRequestedTime',3,['severeImpact','severeCasesByRequestedTime'],0.15,'totalHospitalBeds'],
-        ['impact','casesForICUByRequestedTime',2,['impact','infectionsByRequestedTime'],0.05],
-        ['severeImpact','casesForICUByRequestedTime',2,['severeImpact','infectionsByRequestedTime'],0.05],
-        ['impact','casesForVentilatorsByRequestedTime',2,['impact','infectionsByRequestedTime'],0.02],
-        ['severeImpact','casesForVentilatorsByRequestedTime',2,['severeImpact','infectionsByRequestedTime'],0.02],
-        ['impact','dollarsInFlight',4,['impact','infectionsByRequestedTime','region','avgDailyIncomePopulation','avgDailyIncomeInUSD'],$days],
-        ['severeImpact','dollarsInFlight',4,['severeImpact','infectionsByRequestedTime','region','avgDailyIncomePopulation','avgDailyIncomeInUSD'],$days]);
-    $data=doCompute($data1,$data,$inp1);
-    return $data;
+    $data1['impact']['currentlyInfected']=trunc($data['reportedCases']*10);
+    $data1['severeImpact']['currentlyInfected']=trunc($data['reportedCases']*50);
+    $data1['impact']['infectionsByRequestedTime']=trunc($data1['impact']['currentlyInfected']*pow(2,(int)($days/3)));
+    $data1['severeImpact']['infectionsByRequestedTime']=trunc($data1['severeImpact']['currentlyInfected']*pow(2,(int)($days/3)));
+    $data1['impact']['severeCasesByRequestedTime']=trunc($data1['impact']['infectionsByRequestedTime']*0.15);
+    $data1['severeImpact']['severeCasesByRequestedTime']=trunc($data1['severeImpact']['infectionsByRequestedTime']*0.15);
+    $data1['impact']['hospitalBedsByRequestedTime']=trunc(($data['totalHospitalBeds']*0.35)-$data1['impact']['severeCasesByRequestedTime']);
+    $data1['severeImpact']['hospitalBedsByRequestedTime']=trunc(($data['totalHospitalBeds']*0.35)-$data1['severeImpact']['severeCasesByRequestedTime']);
+    $data1['impact']['casesForICUByRequestedTime']=trunc($data1['impact']['infectionsByRequestedTime']*0.05);
+    $data1['severeImpact']['casesForICUByRequestedTime']=trunc($data1['severeImpact']['infectionsByRequestedTime']*0.05);
+    $data1['impact']['casesForVentilatorsByRequestedTime']=trunc($data1['impact']['infectionsByRequestedTime']*0.02);
+    $data1['severeImpact']['casesForVentilatorsByRequestedTime']=trunc($data1['severeImpact']['infectionsByRequestedTime']*0.02);
+    $data1['impact']['dollarsInFlight']=trunc(($data1['impact']['infectionsByRequestedTime']*$data['region']['avgDailyIncomePopulation']*$data['region']['avgDailyIncomeInUSD'])/$days);
+    $data1['severeImpact']['dollarsInFlight']=trunc(($data1['severeImpact']['infectionsByRequestedTime']*$data['region']['avgDailyIncomePopulation']*$data['region']['avgDailyIncomeInUSD'])/$days);
+    return $data1;
 }
-covid19ImpactEstimator($data);
 
